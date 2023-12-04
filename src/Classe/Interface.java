5ea1734a08470b38685c8077b6d23cf24e814913
Hércules <116934166+thellllima02@users.noreply.github.com>
2023-12-04 15:34:33 -0300

    Quase terminado
    Falta corrigir a parte 6 e terminar a parte oito que esta incompleta

--- src/Classe/Interface.java
+++ src/Classe/Interface.java
@@ -8,12 +8,17 @@ public class Interface {
     public static void main(String[] args) {
         String rodar = " ";
         int opcao = 0;
-        String confirme=" ";
+        String confirme = " ";
         SalvaArquivo sa = new SalvaArquivo();//Salvar os arquivo
         LinkedList<Senha> lista = new LinkedList<Senha>();
         LinkedList<Senha> listaRecupera = new LinkedList<Senha>();
-        listaRecupera  = (LinkedList<Senha>) sa.ler();
-        
+        listaRecupera = (LinkedList<Senha>) sa.ler();//Recupera e add os novos nomes
+        LinkedList<String> listaAplicativoBanco = new LinkedList<String>(); //Receber os nomes de Banco
+        LinkedList<String> listaNomesPassados = new LinkedList<String>(); //Receber os nomes de foram passado
+        String neo2 = "";//Usado para receber os nomes e add na listaAplicativoBanco
+        String neo3 = "";//Usado para receber os nomes e add na listaNomesPassados
+        HashMap<String, LinkedList<String>> mapa = new HashMap<String, LinkedList<String>>();
+        Iterator<Senha> iterador = listaRecupera.iterator();//Questão 5
 
         rodar = JOptionPane.showInputDialog(null, "---- Desejar abrir o sistema ? ----\n ( Sim / Não )");
 
@@ -29,7 +34,6 @@ public class Interface {
                     + "\n7.Salvar Lista "
                     + "\n8.[Extra] Criar índice invertido"
                     + "\n9.Sair ?"));
-            
 
             switch (opcao) {
                 case 1:
@@ -41,53 +45,135 @@ public class Interface {
                     break;
 
                 case 2:
-                    if(listaRecupera.isEmpty()){
+                    if (listaRecupera.isEmpty()) {
                         JOptionPane.showMessageDialog(null, "Está vazia");
-                    }
-                    else{
+                    } else {
                         listaRecupera.removeLast();
                     }
                     break;
 
                 case 3:
-                    if(listaRecupera.isEmpty()){
+                    if (listaRecupera.isEmpty()) {
                         JOptionPane.showMessageDialog(null, "Está vazia");
-                    }
-                    else{
-                        JOptionPane.showMessageDialog(null, "---- Lista gravada ----\n"+listaRecupera.toString());
+                    } else {
+                        JOptionPane.showMessageDialog(null, "---- Lista gravada ----\n" + listaRecupera.toString());
                     }
                     break;
 
                 case 4:
                     Collections.sort(listaRecupera);
-                    JOptionPane.showMessageDialog(null,"Lista em ordena com sucesso\n **Não esqueça de salvar antes de sair**");
-                     break;
+                    JOptionPane.showMessageDialog(null, "** Lista em ordena com sucesso **\n\n **Não esqueça de salvar antes de sair**");
+                    break;
 
                 case 5:
-                    
+                    while (iterador.hasNext()) {
+                        Senha u = iterador.next();
+                        String nome = u.getNomeDoAplicativo();
+                        String senha = u.getSenha();
+
+                        if (!mapa.containsKey(nome)) {
+                            LinkedList<String> pilha = new LinkedList<String>();
+                            mapa.put(nome, pilha);
+                        }
+                        mapa.get(nome).add(senha);
+                    }
+                    JOptionPane.showMessageDialog(null, mapa);
                     break;
 
                 case 6:
-
+                    Arvore arvore = new Arvore();
+                    LinkedList lista2 = new LinkedList();
+                    while (iterador.hasNext()) {
+                        Senha u = iterador.next();
+                        String senha = u.getSenha();
+                        arvore.adicionar(senha);
+                        if (arvore.adicionar(senha) == false) {
+                            lista2.add(senha);
+                        }
+                    }
+                    JOptionPane.showMessageDialog(null,"---- Arvore em Pre Ordem ----\n"+arvore.preOrdem(arvore.getRaiz()));
+                    JOptionPane.showMessageDialog(null,"---- Arvore em Ordem ----\n"+arvore.emOrdem(arvore.getRaiz()));
+                    JOptionPane.showMessageDialog(null,"---- Arvore em Pos Ordem ----\n"+arvore.posOrdem(arvore.getRaiz()));
                     break;
 
                 case 7:
-                   sa.grava(listaRecupera);
-                   JOptionPane.showMessageDialog(null, "---- Lista salva ----");
+                    sa.grava(listaRecupera);
+                    JOptionPane.showMessageDialog(null, "---- Lista salva ----");
                     break;
 
                 case 8:
+                    //Pegando todos os nomes de bancos e colocando em uma lista
+                    for(int e=0; e<listaRecupera.size();e++){
+                        Senha recebe = listaRecupera.get(e);
+                    
+                    String texto = recebe.getNomeDoAplicativo();
+                    
+                    String palavra = "";
+                    int cont = 1,
+                     cont2 = 0;
+                    //Ver quantas palavras tem no frase.
+                    for (int i = 0; i < texto.length(); i++) {
+                        if (texto.charAt(i) == ' ') {
+                            cont++;
+                        }
+                    }
+                    //Fazendo a coleta de cada palavra que veio no texto
+                    for (int i = cont2; i < texto.length(); i++) {
+                        if (texto.charAt(i) != ' ') {
+                            palavra += texto.charAt(i);
+                            cont2++;
+                        } else {
+                            neo2 = new String(palavra);
+                            listaAplicativoBanco.add(neo2);
+                            cont2++;
+                            palavra = "";
+                        }
+                    }
+                    neo2 = new String(palavra);
+                    listaAplicativoBanco.add(neo2);//Usar para ver os nomes de bancos
+                    
+                    }
+                    //Pegando os nomes da String e colocando na lista
+                    String recebe2 = JOptionPane.showInputDialog(null, "Digite os nomes ");
+                    String texto2 = recebe2;
+                    
+                    String palavra2 = "";
+                    int cont = 1,
+                     cont2 = 0;
+                    //Ver quantas palavras tem no frase.
+                    for (int i = 0; i < texto2.length(); i++) {
+                        if (texto2.charAt(i) == ' ') {
+                            cont++;
+                        }
+                    }
+                    //Fazendo a coleta de cada palavra que veio no texto
+                    for (int i = cont2; i < texto2.length(); i++) {
+                        if (texto2.charAt(i) != ' ') {
+                            palavra2 += texto2.charAt(i);
+                            cont2++;
+                        } else {
+                            neo3 = new String(palavra2);
+                            listaNomesPassados.add(neo3);
+                            cont2++;
+                            palavra2 = "";
+                        }
+                    }
+                    neo3 = new String(palavra2);
+                    listaNomesPassados.add(neo3);//Usar para ver os nomes
+                    
+                    JOptionPane.showMessageDialog(null, listaAplicativoBanco);
+                    JOptionPane.showMessageDialog(null, listaNomesPassados);
 
                     break;
 
                 case 9:
-                   confirme = JOptionPane.showInputDialog(null, "---- Você salvou as senhas ? ----\n ( Sim / Não )");
-                   if(confirme.equalsIgnoreCase("sim")){
-                    rodar = "nao";   
-                   }else{
-                       rodar = "sim";
-                   }
-                    
+                    confirme = JOptionPane.showInputDialog(null, "---- Você salvou as senhas ? ----\n ( Sim / Não )");
+                    if (confirme.equalsIgnoreCase("sim")) {
+                        rodar = "nao";
+                    } else {
+                        rodar = "sim";
+                    }
+
                     break;
             }
         }
